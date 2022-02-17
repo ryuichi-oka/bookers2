48ad29670787bf4a31b14ed5662f2001c7a5dffe
EC2 Default User <ec2-user@ip-172-31-42-83.ec2.internal>
2022-02-17 11:19:51 +0000

    [finish]ログインページの編集

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,7 +1,16 @@
 class UsersController < ApplicationController
   def show
+    @user = User.find(params[:id])
+    @books = @user.books
+  end
+
+  def index
+    @users = User.all
   end
 
   def edit
+  end
+
+  def update
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -5,7 +5,7 @@ Rails.application.routes.draw do
 
   resources :books, only: [:new, :create, :index, :show, :edit, :destroy]
 
-  resources :users, only: [:show, :edit, :update]
+  resources :users, only: [:show, :edit, :index, :update]
 
   get 'homes/about' => 'homes#about', as: 'about'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
